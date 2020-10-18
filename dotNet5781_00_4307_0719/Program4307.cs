9ca49753b2eb9d7dd5d35955aa3da011b9ae8952
שי אקסלרוד <Saxelrod@g.jct.ac.il>
2020-10-18 15:27:38 +0300

    Initial code with 2 functions

---
+++ dotNet5781_00_4307_0719/Program4307.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_00_4307_0719
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome4307();
+            Welcome0719();
+        }
+
+        private static void Welcome4307()
+        {
+            Console.WriteLine("Enter your name: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", userName);
+        }
+
+        private static void Welcome0719()
+        { 
+        }
+    }
+}
