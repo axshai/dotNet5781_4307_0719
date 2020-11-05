c797a8955f70e15456ec9103fdab682f1e6dabe0
שי אקסלרוד <Saxelrod@g.jct.ac.il>
2020-11-06 00:10:02 +0200

    functions in Buslines

--- dotNet5781_02_4307_0719/BusLines.cs
+++ dotNet5781_02_4307_0719/BusLines.cs
@@ -8,16 +8,21 @@ namespace dotNet5781_02_4307_0719
 {
     class BusLines
     {
-        private List<BusLineRoute> lines = new List<BusLineRoute>();
+        public BusLines()
+        {
+            Lines = new List<BusLineRoute>();
+        }
+       
+        public List<BusLineRoute> Lines { get; set; }
 
         public void AddOrRemove(string NumberOfLine, int choice)
         {
 
             if (choice == 0)
             {
-                if (lines.Exists(line => line.BusLine == NumberOfLine))
+                if (Lines.Exists(line => line.BusLine == NumberOfLine))
                 {
-                    lines.Remove(lines.Find(line => line.BusLine == NumberOfLine));
+                    Lines.Remove(Lines.Find(line => line.BusLine == NumberOfLine));
                 }
                 else
                 {
@@ -28,20 +33,20 @@ namespace dotNet5781_02_4307_0719
 
             else if (choice == 1)
             {
-                List<BusLineRoute> lines1 = lines.FindAll(line => line.BusLine == NumberOfLine);
-                if (lines1.Count() > 1)
+                List<BusLineRoute> Lines1 = Lines.FindAll(line => line.BusLine == NumberOfLine);
+                if (Lines1.Count() > 1)
                 {
-                    Console.WriteLine("There are identical lines");
+                    Console.WriteLine("There are identical Lines");
                 }
                 else
                 {
                     Console.WriteLine("Enter an area");
                     string area = Console.ReadLine();
-                    if (lines1.Count() < 1 || area == lines1[0].Region.ToString())
+                    if (Lines1.Count() < 1 || area == Lines1[0].Region.ToString())
                     {
                         BusLineRoute NewLine = new BusLineRoute(NumberOfLine, area);
-                        NewLine.AddOrRemove(1, lines1[0].LastStation);
-                        NewLine.AddOrRemove(1, lines1[0].FirstStation, lines1[0].LastStation);
+                        NewLine.AddOrRemove(1, Lines1[0].LastStation);
+                        NewLine.AddOrRemove(1, Lines1[0].FirstStation, Lines1[0].LastStation);
                     }
                     else
                     {
@@ -50,6 +55,38 @@ namespace dotNet5781_02_4307_0719
                 }
             }
         }
+
+        public List<BusLineRoute> BusInStation(string stationNumber)
+        {
+            List<BusLineRoute> buses = Lines.FindAll(line => line.Stations.Exists(station => station.BusStationKey == stationNumber));
+            if (buses.Count == 0)
+                throw new Exception("There are no Lines passing through the station");
+            return buses;
+        }
+
+        public List<BusLineRoute> SortedList(string stationNumber)
+        {
+            List<BusLineRoute> newList = Lines;
+            newList.Sort();
+            return newList;
+        }
+
+        public BusLineRoute this[string index]
+        {
+            set 
+            {
+                if (!Lines.Exists(line => line.BusLine == index))
+                    throw new Exception("The line does not exist");
+                Lines[Lines.FindIndex(line => line.BusLine == index)] = value;
+            }
+            get
+            {
+                if (!Lines.Exists(line => line.BusLine == index))
+                    throw new Exception("The line does not exist");
+                return Lines[Lines.FindIndex(line => line.BusLine == index)];
+            }
+        }
+
     }
 }
 
