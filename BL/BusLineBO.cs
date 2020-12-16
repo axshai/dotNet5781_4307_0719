73d27724b849df72334fef307bec2ff87ad3ec18
itamar zohar <etamarxlll3@gmail.com>
2020-12-16 22:21:55 +0200

    beginning  the "BO"

---
+++ BL/BusLineBO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BO
+{
+    class BusLineBO
+    {
+        public ObservableCollection<LineStationBO> StationList { get; set; }//List of stations for the line
+
+
+
+
+    }
+}
---
+++ DalApi/LineStationDO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DO
+{
+    class LineStation 
+    {
+        public int LineKey { get; set; }
+        public int StationKey { get;set; }
+
+    }
+}
