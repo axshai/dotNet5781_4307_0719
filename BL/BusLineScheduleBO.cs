bd6c2f8009f19fe69c737bc51fed169edfe59e23
itamar zohar <etamarxlll3@gmail.com>
2020-12-21 19:50:20 +0200

    START BL

---
+++ BL/BusLineScheduleBO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BO
+{
+   public class BusLineScheduleBO
+    {
+        public string LineNumber { get; set; }
+        public TimeSpan StartActivity { get; set; }//לא קשור לתאריך מסוים
+        public TimeSpan EndActivity { get; set; }
+        public int frequency { get; set; }
+        public override string ToString()
+        {
+            string result = "LineNumber: " + LineNumber + "\nStartActivity: " + StartActivity.ToString() + "EndActivity: " + EndActivity.ToString() + "frequency: " + frequency.ToString();
+
+            return result;
+        }
+
+    }
+}
---
+++ BL/BusLineStationBO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BO
+{
+    public class BusLineStationBO
+    {
+        public int StationKey { get; set; }
+        public string StationName { get; set; }
+        public double Distance { get; set; }
+        public TimeSpan TineTravel { get; set; }
+
+    }
+}
